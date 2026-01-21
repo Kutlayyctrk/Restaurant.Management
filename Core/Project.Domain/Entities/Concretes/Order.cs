b0f9a71f7059cd02619ca0d48a5359f21e374432
Ktlyctrk <ktlyyctrk@gmail.com>
2026-01-21 18:33:45 +0300

    Order'ı alış ve satışa uygun hale getirmek için OrderType enumu eklendi

--- Core/Project.Domain/Entities/Concretes/Order.cs
+++ Core/Project.Domain/Entities/Concretes/Order.cs
@@ -12,19 +12,19 @@ namespace Project.Domain.Entities.Concretes
     public class Order : BaseEntity
     {
         public int TableId { get; set; }
-        public int? WaiterId { get; set; } //Masaya bakan garson
+        public int? WaiterId { get; set; }
+        public int? SupplierId { get; set; }
 
         public decimal TotalPrice { get; set; }
         public DateTime OrderDate { get; set; }
 
         public OrderStatus OrderState { get; set; }
+        public OrderType Type { get; set; } 
 
-
-
-        //Relational Properties
-
+        // Relational Properties
         public virtual Table Table { get; set; }
         public virtual AppUser Waiter { get; set; }
+        public virtual Supplier Supplier { get; set; }
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
     }
 }
